6643b5601affc79fedcce9c3c9efc0482ac49dd0
Tran Cong Minh <cmisme41200@gmail.com>
2024-09-04 18:25:47 +0700

    Init

--- FE/congminhshop/src/index.js
+++ FE/congminhshop/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import HomePage from './pages/users/homepage/index.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>CongMinh Shop</h1>
+  <HomePage />
 );
 
 
