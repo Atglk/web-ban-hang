828cfc04003891c7574e359f57c441998d35663a
Tran Cong Minh <cmisme41200@gmail.com>
2024-09-05 11:03:20 +0700

    done next

--- FE/congminhshop/src/index.js
+++ FE/congminhshop/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import HomePage from './pages/users/homepage/index.js';
+import Homepage from './pages/users/homepage';
+import { BrowserRouter } from 'react-router-dom';
+import RouterCustom from './router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HomePage />
+  <BrowserRouter>
+    <RouterCustom></RouterCustom>
+  </BrowserRouter>
 );
 
 
